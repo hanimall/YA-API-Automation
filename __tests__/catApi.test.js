8041fc5ebb4599079054b869a6514a7fa3506e9f
Hani Bouras <han.bouras@gmail.com>
2025-04-28 08:39:31 +0100

    Add GET /images/search?breed_ids → should fetch n images for the selected breed

--- __tests__/catApi.test.js
+++ __tests__/catApi.test.js
@@ -1,7 +1,21 @@
 const client = require('../utils/apiClient');
 
-    test('GET /breeds → should fetch list of breeds', async () => {
-      const response = await client.get('/breeds');
-      expect(response.status).toBe(200);
-      expect(Array.isArray(response.data)).toBe(true);
+describe('Breed Endpoints', () => {
+    let breedId;
+
+    test('GET /breeds → should fetch list of breeds and store the first breed ID', async () => {
+        const response = await client.get('/breeds');
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.data)).toBe(true); 
+        breedId = response.data[0].id; // Keep for next test
     });
+
+    test('GET /images/search?breed_ids → should fetch n images for the selected breed', async () => {
+        const limit = 20;
+        const response = await client.get(`/images/search?breed_ids=${breedId}&limit=${limit}`);
+        expect(response.status).toBe(200);
+        expect(response.data.length).toBeGreaterThan(0);
+        expect(response.data[0]).toHaveProperty('breeds');
+        expect(response.data[0].breeds[0].id).toBe(breedId);
+    });
+});
